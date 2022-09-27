fabe9a53fc89a7875fbfcd1feddad5801dfc590b
Sushant <sushant.nalawade@yahoo.in>
2022-09-27 23:52:31 +0530

    Enabling Eureka Client

--- src/main/java/com/movie/infoservice/movie/infoservice/MovieInfoServiceApplication.java
+++ src/main/java/com/movie/infoservice/movie/infoservice/MovieInfoServiceApplication.java
@@ -2,9 +2,24 @@ package com.movie.infoservice.movie.infoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieInfoServiceApplication {
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder getWebClient(){
+		return  WebClient.builder();
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate(){
+		return  new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
--- src/main/java/com/movie/infoservice/movie/infoservice/controller/MovieInfoServiceController.java
+++ src/main/java/com/movie/infoservice/movie/infoservice/controller/MovieInfoServiceController.java
@@ -1,15 +1,36 @@
 package com.movie.infoservice.movie.infoservice.controller;
 
 import com.movie.infoservice.movie.infoservice.pojo.Movie;
+import com.movie.infoservice.movie.infoservice.pojo.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieInfoServiceController {
+
+    @Autowired
+    WebClient.Builder webClientBuilder ;
+    @Autowired
+    RestTemplate restTemplate ;
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId){
-return new Movie(movieId,"Transformers", "Tech Movie");
+RestTemplate restTemplate1 = new RestTemplate();
+        MovieSummary movieSummaryMono = restTemplate1.getForObject("https://api.themoviedb.org/3/movie/25?api_key=5f4a971aefefa3bbbfdb9bd2baff3add", MovieSummary.class);
+     /* MovieSummary movieSummaryMono = webClientBuilder.build()
+                .get()
+                .uri("https://api.themoviedb.org/3/movie/"+movieId+"?api_key=5f4a971aefefa3bbbfdb9bd2baff3add")
+                .retrieve()
+                .bodyToMono(MovieSummary.class)
+                .block();
+*/
+        System.out.println(movieSummaryMono.getId()+"  "+movieSummaryMono.getTitle()+"  "+movieSummaryMono.getOverview());
+        //return movieSummaryMono;
+return new Movie(movieId,movieSummaryMono.getTitle(), movieSummaryMono.getOverview());
     }
 }
